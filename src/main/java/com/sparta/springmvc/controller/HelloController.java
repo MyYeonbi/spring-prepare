f083f6591ff904a0d13179e34f1b04689c18a7d0
MyYeonbi <yeonbi_kim@icloud.com>
2025-03-01 21:50:21 +0900

    ❌ @Controller만 사용하면 View를 반환하려고 해서 404가 났었음. 하지만 @RestController로 수정하니 해결 👍

--- src/main/java/com/sparta/springmvc/controller/HelloController.java
+++ src/main/java/com/sparta/springmvc/controller/HelloController.java
@@ -1,11 +1,17 @@
 package com.sparta.springmvc.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class HelloController {
+
   @GetMapping("/hello")
   @ResponseBody
   public String hello() {
